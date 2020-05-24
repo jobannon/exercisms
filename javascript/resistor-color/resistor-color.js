e2382cb49d64b5f9eb6c69c33543abd53075a85d
Joshua O'bannon <jtobannon@gmail.com>
2020-05-24 12:17:13 -0600

    Complete resistor challengeP

---
+++ javascript/resistor-color/resistor-color.js
@@ -0,0 +1,21 @@
+var color_value_mapper =  {
+    'black': 0, 
+    'brown': 1,
+    'red': 2,
+    'orange': 3,
+    'yellow': 4,
+    'green': 5,
+    'blue': 6,
+    'violet': 7,
+    'grey': 8,
+    'white': 9
+}
+
+export const colorCode = (color) => {
+  
+  return color_value_mapper[color];
+  
+  throw new Error("");
+};
+
+export const COLORS = Object.keys(color_value_mapper);
